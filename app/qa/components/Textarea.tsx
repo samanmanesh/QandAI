60fd96686059283df62e0c9a2ff67bf08664d0e8
Saman <samanmanesh7@gmail.com>
2024-10-14 17:01:55 -0400

    Fixed the text area not showing grammerly suggestions

--- app/qa/components/Textarea.tsx
+++ app/qa/components/Textarea.tsx
@@ -53,14 +53,14 @@ const Textarea = ({ value, onChange, notification }: TextareaProps) => {
         transition={{ duration: 0.7, delay: 0.1 }}
       >
         <textarea
-          // className="w-full aspect-[16/7] resize-none rounded-xl overflow-auto outline-none p-4 shadow-sm bg-gradient-to-tr from-[#221c2cfd] via-[#1b1623f1] to-80% to-[#1b1623d9]   text-white text-base font-medium tracking-wide custom-scrollbar"
-          className="w-full aspect-[16/7] resize-none rounded-xl overflow-auto outline-none p-4   border border-neutral-300 opacity-95 text-base font-medium tracking-wide custom-scrollbar shadow-sm"
+          className=" w-full aspect-[16/7] resize-none rounded-xl overflow-auto outline-none p-4   border border-neutral-300 text-base font-medium tracking-wide custom-scrollbar shadow-sm"
           placeholder="Enter or paste your text here...  "
           onChange={(e) => {
             onChange(e.target.value);
           }}
           value={value}
           ref={textareaRef}
+          autoFocus
         />
         {!notification && (
           <div className="absolute top-full  rounded-md py-1 px-2   font-medium text-lg text-rose-600 ">
